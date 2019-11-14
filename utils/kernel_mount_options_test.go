e09e839f17afca06fd35014bc2cd02fb47ab7f2a
Julian Hjortshoj <jhjortsho@pivotal.io>
2019-11-14 12:22:58 -0800

    Remove redundant ToKernelMountOptionString method
    - Has been moved to smbdriver since only used by smb not nfs
    [#169413129](https://www.pivotaltracker.com/story/show/169413129)
    
    Co-authored-by: Julian Hjortshoj <jhjortsho@pivotal.io>
    Co-authored-by: Lisa Cho <lcho@pivotal.io>

--- utils/kernel_mount_options_test.go
+++ utils/kernel_mount_options_test.go
@@ -1,9 +1,6 @@
 package utils_test
 
 import (
-	"fmt"
-	"math"
-
 	"code.cloudfoundry.org/volume-mount-options/utils"
 
 	. "github.com/onsi/ginkgo"
@@ -11,126 +8,6 @@ import (
 )
 
 var _ = Describe("KernelMountOptions", func() {
-	Describe("#ToKernelMountOptionString", func() {
-		var (
-			mountOpts          map[string]interface{}
-			kernelMountOptions string
-		)
-
-		BeforeEach(func() {
-			mountOpts = make(map[string]interface{})
-		})
-
-		JustBeforeEach(func() {
-			kernelMountOptions = utils.ToKernelMountOptionString(mountOpts)
-		})
-
-		Context("given an empty mount opts", func() {
-			It("should return an empty mount opts string", func() {
-				Expect(kernelMountOptions).To(BeEmpty())
-			})
-		})
-
-		Context("given a mount opts", func() {
-			BeforeEach(func() {
-				mountOpts = map[string]interface{}{
-					"opt1": "val1",
-					"opt2": "val2",
-				}
-			})
-
-			It("should return a valid mount opts string", func() {
-				Expect(kernelMountOptions).To(Equal("opt1=val1,opt2=val2"))
-			})
-		})
-
-		Context("given an integer option value with a leading zero", func() {
-			BeforeEach(func() {
-				mountOpts = map[string]interface{}{
-					"opt1": "0123",
-				}
-			})
-
-			It("strips the leading zero from the mount option string", func() {
-				Expect(kernelMountOptions).To(Equal("opt1=123"))
-			})
-		})
-
-		Context("given an integer option value", func() {
-			BeforeEach(func() {
-				mountOpts = map[string]interface{}{
-					"opt1": math.MaxInt64,
-				}
-			})
-
-			It("strips the leading zero from the mount option string", func() {
-				Expect(kernelMountOptions).To(Equal(fmt.Sprintf("opt1=%d", math.MaxInt64)))
-			})
-		})
-
-		Context("given a mount option with no value", func() {
-			BeforeEach(func() {
-				mountOpts = map[string]interface{}{
-					"does-not-matter": "",
-				}
-			})
-
-			It("contains the mount option", func() {
-				Expect(kernelMountOptions).To(ContainSubstring("does-not-matter"))
-			})
-		})
-
-		Context("given a mount option with nil value", func() {
-			BeforeEach(func() {
-				mountOpts = map[string]interface{}{
-					"does-not-matter": nil,
-				}
-			})
-
-			It("omits the mount option", func() {
-				Expect(kernelMountOptions).NotTo(ContainSubstring("does-not-matter"))
-			})
-		})
-
-		Context("given a 'Domain' mount option with no value", func() {
-			BeforeEach(func() {
-				mountOpts = map[string]interface{}{
-					"domain": "",
-					"Domain": "",
-				}
-			})
-
-			It("omits the mount option", func() {
-				Expect(kernelMountOptions).NotTo(ContainSubstring("domain"))
-				Expect(kernelMountOptions).NotTo(ContainSubstring("Domain"))
-			})
-		})
-
-		Context("given a 'Domain' mount option with nil value", func() {
-			BeforeEach(func() {
-				mountOpts = map[string]interface{}{
-					"domain": nil,
-				}
-			})
-
-			It("omits the mount option", func() {
-				Expect(kernelMountOptions).NotTo(ContainSubstring("domain"))
-			})
-		})
-
-		Context("given a readonly mount option with a string boolean value", func() {
-			BeforeEach(func() {
-				mountOpts = map[string]interface{}{
-					"ro": "true",
-				}
-			})
-
-			It("includes the mount option", func() {
-				Expect(kernelMountOptions).To(ContainSubstring("ro"))
-			})
-		})
-	})
-
 	Describe("#ParseOptionStringToMap", func() {
 		var (
 			optionString string
